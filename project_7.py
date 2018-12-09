9e77c112d04d09e3a58bc2050609adf86d141e8b
XeniaOlshanskaya <ks14012000@gmail.com>
2018-12-09 14:32:31 +0700

    first commit

---
+++ project_7.py
@@ -0,0 +1,14 @@
+print("Вам предоставляется возможность забронировать билеты на авиарейс по следующим направлениям:")
+print("Москва, Астана, Лондон, Берлин, Мадрид, Вашингтон, Рим \n")
+
+list_of_directions = ["москва", "астана", "лондон", "берлин", "мадрид", "вашингтон", "рим"]
+
+list_of_month = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+
+moscow = [list_of_month, ["01", "04", "05"], 'FobosAir', 'MOSCOW, RUSSIA']
+astana = [list_of_month, ["01", "04", "05"], 'KazAir', 'ASTANA, KAZAKHSTAN']
+london = [list_of_month, ["01", "04", "05"], 'Aeroplane', 'LONDON, UK']
+berlin = [list_of_month, ["01", "04", "05"], 'Aeroplane', 'BERLIN, GERMANY']
+madrid = [list_of_month, ["01", "04", "05"], 'FobosAir', 'MADRID, SPAIN']
+washington = [list_of_month, ["01", "04", "05"], 'AmericanAirlines', 'WASHINGTON DC, USA']
+roma = [list_of_month, ["01", "04", "05"], 'FobosAir', 'ROMA, ITALY']
